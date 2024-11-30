0f3a915e7c06d6ae49432085fbc4d642db40a6e0
git-dongjin <y2kf9a0q@gmail.com>
2024-11-30 21:28:29 +0900

    feature: Test코드에 Mock 주입

--- src/test/java/com/sesac/backend/BackendApplicationTests.java
+++ src/test/java/com/sesac/backend/BackendApplicationTests.java
@@ -1,10 +1,21 @@
 package com.sesac.backend;
 
+import com.sesac.backend.auths.utils.JwtAuthenticationFilter;
+import com.sesac.backend.auths.utils.JwtTokenProvider;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BackendApplicationTests {
+
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+
+    @MockBean
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Test
     void contextLoads() {
