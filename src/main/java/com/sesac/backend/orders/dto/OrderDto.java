a5ee847fd19cf51a87c5bf8740595ba4255f9072
git-dongjin <y2kf9a0q@gmail.com>
2024-12-02 18:53:09 +0900

    basecode: orders

---
+++ src/main/java/com/sesac/backend/orders/dto/OrderDto.java
@@ -0,0 +1,20 @@
+package com.sesac.backend.orders.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderDto {
+
+    private Long id;
+}
