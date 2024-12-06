07fe1df452413003b212451ab8f212241cb12195
git-dongjin <y2kf9a0q@gmail.com>
2024-12-06 15:01:32 +0900

    feature: Quiz 엔티티 생성

---
+++ src/main/java/com/sesac/backend/quizProblems/domain/QuizProblem.java
@@ -0,0 +1,22 @@
+package com.sesac.backend.quizProblems.domain;
+
+import com.sesac.backend.quizzes.domain.Quiz;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class QuizProblem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id", nullable = false)
+    private Quiz quiz;
+}
