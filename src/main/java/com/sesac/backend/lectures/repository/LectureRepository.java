836bd6ad06002cf32fdcf4097250c0590de8723b
hip1hip <honginpyo2100@gmail.com>
2024-12-06 19:50:34 +0900

    feat: 메인 pull 전 푸쉬 - Lecture 수저완

--- src/main/java/com/sesac/backend/lectures/repository/LectureRepository.java
+++ src/main/java/com/sesac/backend/lectures/repository/LectureRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
-    Optional<Lecture> findByVideoKey(String videoKey);
+    // Optional<Lecture> findByVideoKey(String videoKey);
 }
