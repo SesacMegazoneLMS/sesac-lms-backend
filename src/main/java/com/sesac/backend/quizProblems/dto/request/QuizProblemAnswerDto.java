633ffaad231beebc767e66d86606122051601de8
git-dongjin <y2kf9a0q@gmail.com>
2024-12-08 22:24:08 +0900

    feature: 응시자가 제출한 응답 dto 생성

---
+++ src/main/java/com/sesac/backend/quizProblems/dto/request/QuizProblemAnswerDto.java
@@ -0,0 +1,25 @@
+package com.sesac.backend.quizProblems.dto.request;
+
+import com.sesac.backend.quizProblems.enums.Answer;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QuizProblemAnswerDto {
+
+    private Long id;
+
+    private Integer problemNumber;
+
+    private Answer selectedAnswer;
+}
