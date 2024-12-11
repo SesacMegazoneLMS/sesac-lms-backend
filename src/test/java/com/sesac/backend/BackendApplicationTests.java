baddb14ac8c9c9e4e1fc41908e60cd5857fc18ba
git-dongjin <148426511+git-dongjin@users.noreply.github.com>
2024-12-11 17:35:18 +0900

    Update BackendApplicationTests.java

--- src/test/java/com/sesac/backend/BackendApplicationTests.java
+++ src/test/java/com/sesac/backend/BackendApplicationTests.java
@@ -7,20 +7,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(classes = {BackendApplication.class})
-@TestPropertySource(properties = "spring.profiles.active=test")
-@EnableAutoConfiguration(exclude = {AwsConfig.class})
+@SpringBootTest
 @AutoConfigureMockMvc
 class BackendApplicationTests {
 
     @MockBean
     private JwtTokenProvider jwtTokenProvider;
-
     @MockBean
     private JwtAuthenticationFilter jwtAuthenticationFilter;
-
     @Test
     void contextLoads() {
     }
-
 }
